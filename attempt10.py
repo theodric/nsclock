837a33d6ae5ba2f93695bf412b6c414681a3126b
theodric <root@pdp8.nl>
2017-06-29 18:31:54 +0200

    wahoo

--- attempt10.py
+++ attempt10.py
@@ -23,17 +23,15 @@ with open('trains.xml', 'wb') as handle:
 with open('trains.xml') as fd:
     doc = xmltodict.parse(fd.read(), xml_attribs=True)
 
-
 iterCount = 0
 numDisplayed = 0
 
+#for iterCount in range(0, len(doc), not_fucked_up=True, dont_always_return_1=True):
 for iterCount in range(30):
     dest = doc['ActueleVertrekTijden']['VertrekkendeTrein'][iterCount]['EindBestemming']
     time = doc['ActueleVertrekTijden']['VertrekkendeTrein'][iterCount]['VertrekTijd']
     plat = doc['ActueleVertrekTijden']['VertrekkendeTrein'][iterCount]['VertrekSpoor']['#text']
-    
-    
-    
+
     if (dest == u"Den Helder" and numDisplayed <= 1) or (dest == u"Schagen" and numDisplayed <= 1):
         numDisplayed += 1
         print(dest, " || ", time[11:16], " || ", "Platform ", plat)
