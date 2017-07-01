29f620c429228f9aa7f59ef6631c365d7a345acf
theodric <theodric@github.io>
2017-07-01 19:00:09 +0200

    wahoo

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # nsclock
-## NS Nederlandse Spoorwegen clock
-At some point. In theory. If I ever understand the first thing about Python.
+## NS Nederlandse Spoorwegen clocks
 
+<<<<<<< HEAD
+* pip install -r requirements.txt
+
+clockapp.py:
+
+
+cliclock.py:
+Prints 
+
+=======
+>>>>>>> a6dc40c1db6758f1106824b3ff86bd4d366b7b40
--- cliclock.py
+++ cliclock.py
@@ -1,9 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2.7
 
 import sys
 import requests
 import xmltodict
 from collections import OrderedDict
+
+## This script makes use of the NS API, documented extensively here:
+## http://www.ns.nl/en/travel-information/ns-api
+
+## Look up what 
+
+## Hardcode your default stations here.
+station1 = "Den Helder"
+station2 = "Schagen"
+
+## There are two destinations that get me
+## to my target station, so I'm checking for two, but you can just uncomment
+## the next line if you only need one.
+# station2 = station1
+
 
 try:
     import settings
@@ -16,11 +31,11 @@ response = requests.get('http://webservices.ns.nl/ns-api-avt?station=asd',
                 settings.username,
                 settings.apikey), stream=True)
 
-with open('trains.xml', 'wb') as handle:
+with open('/tmp/trains.xml', 'wb') as handle:
     for block in response.iter_content(1024):
         handle.write(block)
 
-with open('trains.xml') as fd:
+with open('/tmp/trains.xml') as fd:
     doc = xmltodict.parse(fd.read(), xml_attribs=True)
 
 iterCount = 0
@@ -32,6 +47,6 @@ for iterCount in range(30):
     time = doc['ActueleVertrekTijden']['VertrekkendeTrein'][iterCount]['VertrekTijd']
     plat = doc['ActueleVertrekTijden']['VertrekkendeTrein'][iterCount]['VertrekSpoor']['#text']
 
-    if (dest == u"Den Helder" and numDisplayed <= 1) or (dest == u"Schagen" and numDisplayed <= 1):
+    if (dest == str(station1) and numDisplayed <= 1) or (dest == (station2) and numDisplayed <= 1):
         numDisplayed += 1
         print(dest, " || ", time[11:16], " || ", "Platform ", plat)
