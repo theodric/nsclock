cf3014d65e1ff03be4c70709b1a10512f0ab7914
theodric <theodric@github.io>
2017-07-01 16:37:54 +0200

    wahoo

--- clockapp.py
+++ clockapp.py
@@ -116,7 +116,7 @@ def main():
         text.AddText("Vertrek van de treinen\n\n", 10, 0, 13, Id="Header")
 #        text.AddText("Apparently there", 15, 35, 18, Id="errtxt1")
 #        text.AddText("are no trains.", 25, 55, 18, Id="errtxt2")
-        text.AddText("   . . . . o o o o o", 0, 10, 13, Id="Header")
+        text.AddText("   . .DERP . . o o o o o", 0, 10, 13, Id="Header")
         text.AddText(train2, 0, 20, 13, Id="Header")
         text.AddText(train3, 0, 30, 13, Id="Header")
         text.AddText(train4, 0, 40, 13, Id="Header")
